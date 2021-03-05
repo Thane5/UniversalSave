3028dc158138bb5117f3fa65cbd6ac92efcfea2d
Christoph Lohr <clohrffb@gmail.com>
2021-03-05 09:48:46 +0100

    Added specific units to save Vector3, Boolean and String values

---
+++ Runtime/Units/SetStringVariable.cs
@@ -0,0 +1,70 @@
+﻿using Ludiq;
+using Bolt;
+
+namespace Lasm.Bolt.UniversalSaver
+{
+    /// <summary>
+    /// A Unit that sets a variable value of a UniversalSave instance.
+    /// </summary>
+    [UnitCategory("IO")]
+    [RenamedFrom("Lasm.BoltExtensions.IO.SetBinaryVariable")]
+    [RenamedFrom("Lasm.BoltExtensions.SetBinaryVariable")]
+    [RenamedFrom("Lasm.UAlive.SetBinaryVariable")]
+    public sealed class SetStringVariable : UniversalSaveUnit
+    {
+        /// <summary>
+        /// The Control Input port we enter when we want to set a variable.
+        /// </summary>
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlInput enter;
+
+        /// <summary>
+        /// The Control Output port invoked when setting the variable is complete.
+        /// </summary>
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlOutput exit;
+
+        /// <summary>
+        /// The Value Input port for the instance of the Universal Save we are setting the variable on.
+        /// </summary>
+        [DoNotSerialize]
+        public ValueInput binary;
+
+        /// <summary>
+        /// The name of this variable.
+        /// </summary>
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput variableName;
+
+        /// <summary>
+        /// The value for this variable.
+        /// </summary>
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput value;
+
+        protected override void Definition()
+        {
+            enter = ControlInput("enter", SetVariable);
+            binary = ValueInput<UniversalSave>(nameof(binary));
+            variableName = ValueInput<string>(nameof(variableName), string.Empty);
+            value = ValueInput<string>(nameof(value), "");
+            exit = ControlOutput("exit");
+
+            Requirement(binary, enter);
+            Requirement(value, enter);
+            Requirement(variableName, enter);
+
+            Succession(enter, exit);
+        }
+
+        private ControlOutput SetVariable(Flow flow)
+        {
+            flow.GetValue<UniversalSave>(binary).Set(flow.GetValue<string>(variableName), flow.GetValue<object>(value));
+            return exit;
+        }
+    }
+}
---
+++ Runtime/Units/SetVector3Variable.cs
@@ -0,0 +1,71 @@
+﻿using Ludiq;
+using Bolt;
+using UnityEngine;
+
+namespace Lasm.Bolt.UniversalSaver
+{
+    /// <summary>
+    /// A Unit that sets a variable value of a UniversalSave instance.
+    /// </summary>
+    [UnitCategory("IO")]
+    [RenamedFrom("Lasm.BoltExtensions.IO.SetBinaryVariable")]
+    [RenamedFrom("Lasm.BoltExtensions.SetBinaryVariable")]
+    [RenamedFrom("Lasm.UAlive.SetBinaryVariable")]
+    public sealed class SetVector3Variable : UniversalSaveUnit
+    {
+        /// <summary>
+        /// The Control Input port we enter when we want to set a variable.
+        /// </summary>
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlInput enter;
+
+        /// <summary>
+        /// The Control Output port invoked when setting the variable is complete.
+        /// </summary>
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlOutput exit;
+
+        /// <summary>
+        /// The Value Input port for the instance of the Universal Save we are setting the variable on.
+        /// </summary>
+        [DoNotSerialize]
+        public ValueInput binary;
+
+        /// <summary>
+        /// The name of this variable.
+        /// </summary>
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput variableName;
+
+        /// <summary>
+        /// The value for this variable.
+        /// </summary>
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput value;
+
+        protected override void Definition()
+        {
+            enter = ControlInput("enter", SetVariable);
+            binary = ValueInput<UniversalSave>(nameof(binary));
+            variableName = ValueInput<string>(nameof(variableName), string.Empty);
+            value = ValueInput<Vector3>(nameof(value), Vector3.zero);
+            exit = ControlOutput("exit");
+
+            Requirement(binary, enter);
+            Requirement(value, enter);
+            Requirement(variableName, enter);
+
+            Succession(enter, exit);
+        }
+
+        private ControlOutput SetVariable(Flow flow)
+        {
+            flow.GetValue<UniversalSave>(binary).Set(flow.GetValue<string>(variableName), flow.GetValue<object>(value));
+            return exit;
+        }
+    }
+}
